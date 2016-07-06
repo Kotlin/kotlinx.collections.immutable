cd75e6d7d5872422bad87a355246c0d49ea8eba3
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-07-06 18:44:53 +0300

    Java tests for ImmutableList.

---
+++ kotlinx-collections-immutable/tests/src/test/kotlin/kotlinx.collections.immutable/ImmutableListJavaTest.java
@@ -0,0 +1,27 @@
+package kotlinx.collections.immutable;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+import static kotlinx.collections.immutable.ExtensionsKt.immutableListOf;
+import static kotlin.collections.CollectionsKt.listOf;
+
+public class ImmutableListJavaTest {
+
+    @Test
+    public void immutableList() {
+        ImmutableList<String> list = immutableListOf("x");
+
+        Assert.assertEquals(listOf("x"), list);
+
+        list = list.clear();
+        Assert.assertEquals(immutableListOf(), list);
+
+        list = list.add("x").add("z").set(0, "y");
+        Assert.assertEquals(listOf("y", "z"), list);
+
+        list = list.remove("y").addAll(list);
+        Assert.assertEquals(listOf("z", "y", "z"), list);
+    }
+}
