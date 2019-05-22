8a23993b621e5c8bf111d875e28f3806d5ff7450
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2019-05-22 14:51:37 +0300

    Kotlin immutable collections benchmarks

---
+++ benchmarks-mpp/src/jvmMain/kotlin/benchmarks/immutableList/builder/Get.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2016-2019 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package benchmarks.immutableList.builder
+
+import benchmarks.*
+import kotlinx.collections.immutable.persistentListOf
+import org.openjdk.jmh.annotations.*
+import org.openjdk.jmh.infra.Blackhole
+import java.util.concurrent.TimeUnit
+
+@Fork(1)
+@Warmup(iterations = 5)
+@Measurement(iterations = 5)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@State(Scope.Benchmark)
+open class Get {
+    @Param(BM_1, BM_10, BM_100, BM_1000, BM_10000, BM_100000, BM_1000000, BM_10000000)
+    var size: Int = 0
+
+    @Param(IP_100, IP_99_09, IP_95, IP_70, IP_50, IP_30, IP_0)
+    var immutablePercentage: Double = 0.0
+
+    private var builder = persistentListOf<String>().builder()
+
+    @Setup(Level.Trial)
+    fun prepare() {
+        builder = persistentListBuilderAdd(size, immutablePercentage)
+    }
+
+    @Benchmark
+    fun getByIndex(bh: Blackhole) {
+        for (i in 0 until builder.size) {
+            bh.consume(builder[i])
+        }
+    }
+}
---
+++ benchmarks-mpp/src/jvmMain/kotlin/benchmarks/immutableMap/Remove.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2016-2019 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package benchmarks.immutableMap
+
+import benchmarks.*
+import kotlinx.collections.immutable.PersistentMap
+import kotlinx.collections.immutable.persistentMapOf
+import org.openjdk.jmh.annotations.*
+import java.util.concurrent.TimeUnit
+
+@Fork(1)
+@Warmup(iterations = 5)
+@Measurement(iterations = 5)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@State(Scope.Thread)
+open class Remove {
+    @Param(BM_1, BM_10, BM_100, BM_1000, BM_10000, BM_100000, BM_1000000)
+    var size: Int = 0
+
+    @Param(HASH_IMPL, ORDERED_IMPL)
+    var implementation = ""
+
+    @Param(ASCENDING_HASH_CODE, RANDOM_HASH_CODE, COLLISION_HASH_CODE, NON_EXISTING_HASH_CODE)
+    var hashCodeType = ""
+
+    private var keys = listOf<IntWrapper>()
+    private var persistentMap = persistentMapOf<IntWrapper, String>()
+
+    @Setup(Level.Trial)
+    fun prepare() {
+        keys = generateKeys(hashCodeType, size)
+        persistentMap = persistentMapPut(implementation, keys)
+
+        if (hashCodeType == NON_EXISTING_HASH_CODE)
+            keys = generateKeys(hashCodeType, size)
+    }
+
+    @Benchmark
+    fun remove(): PersistentMap<IntWrapper, String> {
+        var map = persistentMap
+        repeat(times = size) { index ->
+            map = map.remove(keys[index])
+        }
+        return map
+    }
+}
