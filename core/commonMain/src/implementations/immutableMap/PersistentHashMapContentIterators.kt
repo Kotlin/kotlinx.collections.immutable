79cbf64d64b928868599e00f5629574be793e9dc
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2019-09-12 20:28:35 +0300

    Update copyright notice in files

--- core/commonMain/src/implementations/immutableMap/PersistentHashMapContentIterators.kt
+++ core/commonMain/src/implementations/immutableMap/PersistentHashMapContentIterators.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2016-2019 JetBrains s.r.o.
+ * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
+ */
+
 package kotlinx.collections.immutable.implementations.immutableMap
 
 import kotlinx.collections.immutable.internal.assert
