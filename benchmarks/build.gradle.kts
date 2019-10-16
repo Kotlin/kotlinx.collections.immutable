5ebaf66cc552cb6e6e1e3405ee64eda726bcf432
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2019-10-16 17:57:06 +0300

    Place runner inside benchmarks project

--- benchmarks/build.gradle.kts
+++ benchmarks/build.gradle.kts
@@ -1,19 +1,3 @@
-/*
- * Copyright 2016-2019 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import kotlinx.benchmark.gradle.JvmBenchmarkTarget
 import org.gradle.jvm.tasks.Jar
 
@@ -55,8 +39,8 @@ kotlin {
     }
 
     sourceSets.all {
-        kotlin.setSrcDirs(listOf("$name/src"))
-        resources.setSrcDirs(listOf("$name/resources"))
+        kotlin.setSrcDirs(listOf("src/$name/kotlin"))
+        resources.setSrcDirs(listOf("src/$name/resources"))
     }
 
     sourceSets {
