719573c6bb68090a9e47b0809d73fbb47364392f
Goooler <wangzongler@gmail.com>
2024-08-21 10:01:30 +0800

    Convert all Gradle build scripts to kts

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlinx/maven")
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "Kotlin-Immutable-Collections" // TODO: Make readable name when it's not used in js module names
+
+include(":core")
+project(":core").name="kotlinx-collections-immutable"
+
+include(
+    ":benchmarks",
+    ":benchmarks:runner"
+)
