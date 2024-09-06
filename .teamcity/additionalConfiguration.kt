d6480e02b1f85fc506522ec93fc8d8f04f8ac5a6
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2024-09-06 16:08:09 +0300

    Show TeamCity build status GitHub PRs

--- .teamcity/additionalConfiguration.kt
+++ .teamcity/additionalConfiguration.kt
@@ -4,7 +4,21 @@
  */
 
 import jetbrains.buildServer.configs.kotlin.Project
+import jetbrains.buildServer.configs.kotlin.DslContext
+import jetbrains.buildServer.configs.kotlin.buildFeatures.commitStatusPublisher
 
 fun Project.additionalConfiguration() {
     subProject(benchmarksProject(knownBuilds.buildVersion))
+
+    knownBuilds.buildAll.features {
+        commitStatusPublisher {
+            vcsRootExtId = "${DslContext.settingsRoot.id}"
+            publisher = github {
+                githubUrl = "https://api.github.com"
+                authType = storedToken {
+                    tokenId = "tc_token_id:CID_7db3007c46f7e30124f81ef54591b223:-1:f604c3ec-6391-4e29-a6e4-e59397b4622d"
+                }
+            }
+        }
+    }
 }
