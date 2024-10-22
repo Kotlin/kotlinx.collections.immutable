b15a09f5ea314d865cd8a63f8e4dfc582848a876
Margarita Bobova <margarita.bobova@jetbrains.com>
2024-10-22 10:43:12 +0200

    Add kotlin repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,10 @@ pluginManagement {
     repositories {
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlinx/maven")
         gradlePluginPortal()
+        val kotlinRepoUrl: String? = providers.gradleProperty("kotlin_repo_url").orNull
+        if (kotlinRepoUrl?.isNotEmpty() == true) {
+            maven(kotlinRepoUrl)
+        }
     }
 }
 
