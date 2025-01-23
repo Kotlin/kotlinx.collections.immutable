19e531086a1b6172db19ad14c10dff9c1ca09e6f
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2025-01-23 16:14:55 -0500

    Add a config to run tests in TC against Kotlin master

--- .teamcity/additionalConfiguration.kt
+++ .teamcity/additionalConfiguration.kt
@@ -1,11 +1,11 @@
 /*
- * Copyright 2016-2020 JetBrains s.r.o.
+ * Copyright 2016-2025 JetBrains s.r.o.
  * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
  */
 
-import jetbrains.buildServer.configs.kotlin.Project
-import jetbrains.buildServer.configs.kotlin.DslContext
+import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.commitStatusPublisher
+import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
 
 fun Project.additionalConfiguration() {
     subProject(benchmarksProject(knownBuilds.buildVersion))
@@ -21,4 +21,57 @@ fun Project.additionalConfiguration() {
             }
         }
     }
+
+    // Check with Kotlin master only on Linux
+    buildWithKotlinMaster(Platform.Linux, knownBuilds.buildVersion).also {
+        knownBuilds.buildAll.dependsOnSnapshot(it, onFailure = FailureAction.ADD_PROBLEM)
+    }
 }
+
+fun Project.buildWithKotlinMaster(platform: Platform, versionBuild: BuildType) = BuildType {
+    id("Build_with_Kotlin_Master_Linux")
+    this.name = "Build with Kotlin Master (${platform.buildTypeName()})"
+
+    requirements {
+        contains("teamcity.agent.jvm.os.name", platform.teamcityAgentName())
+    }
+    commonConfigure()
+
+    dependsOnSnapshot(versionBuild)
+    params {
+        param(versionSuffixParameter, versionBuild.depParamRefs[versionSuffixParameter].ref)
+        param(teamcitySuffixParameter, versionBuild.depParamRefs[teamcitySuffixParameter].ref)
+    }
+
+    val kotlinVersionParameter = "dep.Kotlin_KotlinPublic_BuildNumber.deployVersion"
+
+    dependsOn(AbsoluteId("Kotlin_KotlinPublic_Artifacts")) {
+        artifacts {
+            buildRule = lastSuccessful()
+            cleanDestination = true
+            artifactRules = "+:maven.zip!**=>artifacts/kotlin"
+        }
+    }
+
+    steps {
+        gradle {
+            name = "Build and Test ${platform.buildTypeName()} Binaries"
+            jdkHome = "%env.$jdk%"
+            jvmArgs = "-Xmx1g"
+            tasks = "clean publishToBuildLocal check"
+            // --continue is needed to run tests for all targets even if one target fails
+            gradleParams = listOf(
+                "-x kotlinStoreYarnLock",
+                "--info", "--stacktrace", "--continue",
+                "-P$versionSuffixParameter=%$versionSuffixParameter%", "-P$teamcitySuffixParameter=%$teamcitySuffixParameter%",
+                "-Pkotlin_repo_url=file://%teamcity.build.checkoutDir%/artifacts/kotlin",
+                "-Pkotlin_version=%$kotlinVersionParameter%", "-Pkotlin.native.version=%$kotlinVersionParameter%"
+            ).joinToString(separator = " ")
+            buildFile = ""
+            gradleWrapperPath = ""
+        }
+    }
+
+    // What files to publish as build artifacts
+    artifactRules = "+:build/maven=>maven\n+:build/api=>api\n+:artifacts"
+}.also { buildType(it) }
