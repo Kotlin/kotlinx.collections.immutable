a6d39a58e3f320df19803daa5b2692bc41f07a49
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-04-14 13:46:15 +0400

    204: PersistentOrderedSetTest.equalsTestFromGitHubIssue

---
+++ core/commonTest/src/stress/set/PersistentOrderedSetTest.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2016-2025 JetBrains s.r.o.
+ * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
+ */
+
+package tests.stress.set
+
+import kotlinx.collections.immutable.persistentSetOf
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class PersistentOrderedSetTest {
+
+    @Test
+    fun equalsTestFromGitHubIssue() {
+        val set1 = persistentSetOf(-486539264, 16777216, 0, 67108864)
+        val builder = set1.builder()
+
+        assertEquals(set1, builder.build().toSet())
+        assertEquals(set1, builder.build())
+
+        val set2 = set1.remove(0)
+        builder.remove(0)
+
+        assertEquals(set2, builder.build().toSet())
+        assertEquals(set2, builder.build())
+    }
+}
