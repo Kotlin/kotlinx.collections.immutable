6b3feba23dc769c0134cf9d5c06d22c2a7c423f8
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-04-18 16:22:52 +0400

    204: Rollback return in makeNode

--- core/commonMain/src/implementations/immutableMap/TrieNode.kt
+++ core/commonMain/src/implementations/immutableMap/TrieNode.kt
@@ -292,8 +292,7 @@ internal class TrieNode<K, V>(
         if (shift > MAX_SHIFT) {
 //            assert(key1 != key2)
             // when two key hashes are entirely equal: the last level subtrie node stores them just as unordered list
-            val node = TrieNode<K, V>(0, 0, arrayOf(key1, value1, key2, value2), owner)
-            return node
+            return TrieNode(0, 0, arrayOf(key1, value1, key2, value2), owner)
         }
 
         val setBit1 = indexSegment(keyHash1, shift)
