95c399eb938afb604c315c94e92765198f274303
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-04-20 14:25:33 +0400

    204: Refactor testReproduceOverIterationIssue

--- core/commonTest/src/stress/map/PersistentHashMapBuilderTest.kt
+++ core/commonTest/src/stress/map/PersistentHashMapBuilderTest.kt
@@ -232,7 +232,8 @@ class PersistentHashMapBuilderTest : ExecutionTimeMeasuringTest() {
     fun testReproduceOverIterationIssue() {
         val map: PersistentHashMap<Int, String> =
             persistentHashMapOf(1 to "a", 2  to "b", 3 to "c", 0 to "y", 32 to "z") as PersistentHashMap<Int, String>
-        val iterator = map.builder().entries.iterator()
+        val builder = map.builder()
+        val iterator = builder.entries.iterator()
 
         val expectedCount = map.size
         var actualCount = 0
