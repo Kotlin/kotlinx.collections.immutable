6e0ebeddb7188d2b6bdde7cf591d1cf7695d1510
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-04-25 18:10:42 +0400

    204: Rename tests

--- core/commonTest/src/stress/map/PersistentHashMapTest.kt
+++ core/commonTest/src/stress/map/PersistentHashMapTest.kt
@@ -353,7 +353,7 @@ class PersistentHashMapTest : ExecutionTimeMeasuringTest() {
     }
 
     @Test
-    fun valuePromotionAfterMutableRemovingTest() {
+    fun `if the collision is of size 2 and one of the keys is removed, the remaining key must be promoted`() {
         val map1: PersistentHashMap<Int, String> =
             persistentHashMapOf(-1 to "a", 0 to "b", 32 to "c") as PersistentHashMap<Int, String>
         val builder = map1.builder()
--- core/commonTest/src/stress/set/PersistentOrderedSetTest.kt
+++ core/commonTest/src/stress/set/PersistentOrderedSetTest.kt
@@ -16,7 +16,7 @@ class PersistentOrderedSetTest {
      * Test from issue: https://github.com/Kotlin/kotlinx.collections.immutable/issues/204
      */
     @Test
-    fun persistentSetAndBuilderEqualityBeforeAndAfterModificationTest() {
+    fun `persistentSet and their builder should be equal before and after modification`() {
         val set1 = persistentSetOf(-486539264, 16777216, 0, 67108864)
         val builder = set1.builder()
 
