a47327e40605f569423dc4fa7cacd77883be1d95
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-10-24 14:59:43 +0400

    Deprecate `addAll()` in favor of `addingAll()` across persistent collections and update relevant usages

--- core/jvmTest/src/contract/set/PersistentSetGenerator.kt
+++ core/jvmTest/src/contract/set/PersistentSetGenerator.kt
@@ -20,7 +20,7 @@ class PersistentSetGenerator {
 
         object AddAll : TestStringSetGenerator() {
             override fun create(elements: Array<out String>): Set<String> {
-                return persistentHashSetOf<String>().addAll(elements.toList())
+                return persistentHashSetOf<String>().addingAll(elements.toList())
             }
         }
 
@@ -74,7 +74,7 @@ class PersistentSetGenerator {
 
         object AddAll : TestStringSetGenerator() {
             override fun create(elements: Array<out String>): Set<String> {
-                return persistentSetOf<String>().addAll(elements.toList())
+                return persistentSetOf<String>().addingAll(elements.toList())
             }
         }
 
