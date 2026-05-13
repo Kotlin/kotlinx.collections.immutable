6c376b9046c2190da6dc3bc033632324b71a4e77
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2026-05-13 13:37:25 -0400

    Fix DeploymentId issue

--- .teamcity/additionalConfiguration.kt
+++ .teamcity/additionalConfiguration.kt
@@ -24,7 +24,4 @@ fun Project.additionalConfiguration() {
     val deploymentProject = knownBuilds.deploymentSubproject
     val startTask = deploymentProject.knownBuilds.deployStart
     startTask.params.param("reverse.dep.*.DeploymentName", "kotlinx.collections.immutable %releaseVersion%")
-    deploymentProject.knownBuilds.deployPublish.params {
-        param("DeploymentId", "${deploymentProject.knownBuilds.deployUpload.depParamRefs["output.DeploymentId"]}")
-    }
 }
