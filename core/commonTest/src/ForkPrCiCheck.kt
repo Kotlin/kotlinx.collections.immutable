fde163b21b1fb810b53dd41966d19d24e0014be6
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2026-07-02 15:45:24 +0400

    Temporary: verify fork-PR CI flow (will not be merged)

---
+++ core/commonTest/src/ForkPrCiCheck.kt
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2016-2026 JetBrains s.r.o.
+ * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
+ */
+
+// Temporary marker for verifying the fork-PR CI flow (#265). Not for merge.
+@Suppress("unused")
+private const val forkPrCiCheckMarker = "issue-265-fork-pr-check"
